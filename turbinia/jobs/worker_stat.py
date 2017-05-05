e215f55131f69d2be3adab8171b6e9a639f1b00b
Aaron Peterson <aaronp@gmail.com>
2017-05-04 18:01:59 -0700

    Add new worker stat job/task.  Right now this is just for a simple test, but later we can use this for worker health checks that don't exist in PSQ

---
+++ turbinia/jobs/worker_stat.py
@@ -0,0 +1,41 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Job for running a test evidence stat call on the supplied evidence."""
+
+from turbinia.evidence import RawDisk
+from turbinia.evidence import ReportText
+from turbinia.jobs import TurbiniaJob
+from turbinia.workers.worker_stat import StatTask
+
+
+class StatJob(TurbiniaJob):
+  """Job to run Stat."""
+
+  # The types of evidence that this Job will process
+  evidence_input = [type(RawDisk())]
+  evidence_output = [type(ReportText())]
+
+  def __init__(self):
+    super(StatJob, self).__init__(name='StatJob')
+
+  def create_tasks(self, evidence):
+    """Create task for Stat.
+
+    Args:
+      evidence: List of evidence object to process
+
+    Returns:
+        A list of StatTasks.
+    """
+    return [StatTask(e) for e in evidence]
