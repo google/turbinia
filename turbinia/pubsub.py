205b906a8c52422cf1fec2bb1e688170f0647731
Aaron Peterson <aaronp@gmail.com>
2017-06-06 15:49:28 -0700

    Add pubsub front-end interface.

--- turbinia/pubsub.py
+++ turbinia/pubsub.py
@@ -11,154 +11,168 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Google PubSub Client."""
+"""Google PubSub Listener for requests to Turbinia to process evidence."""
 
-import base64
-import httplib2
+import copy
 import json
 import logging
+import uuid
 
-# Google API
-from googleapiclient import discovery
-from googleapiclient import errors as google_errors
-from googleapiclient import http as storage_http
-from oauth2client import client as oauth2client
+from google.cloud import pubsub
 
 # Turbinia
 from turbinia import config
+from turbinia import evidence
+from turbinia import TurbiniaException
 
 log = logging.getLogger('turbinia')
 
-# PubSub Message types
-[
-    # Messages sent to server
-    ARTIFACTNEW,
-    WORKERUPDATE,
-    TASKUPDATE,
-    # Messages sent to workers
-    TASKSTART,
-    TASKSTOP,] = xrange(5)
 
+class TurbiniaRequest(object):
+  """An object to request evidence to be processed.
 
-class GoogleCloudClient(object):
+  Attributes:
+    request_id: A client specified ID for this request.
+    recipe: Recipe to use when processing this request.
+    context: A Dict of context data to be passed around with this request.
+    evidence: A list of Evidence objects.
+  """
 
-  def __init__(self, service):
-    self.service = service
-    self.client = None
+  def __init__(self, request_id=None, recipe=None, context=None,
+               evidence_=None):
+    """Initialization for TurbiniaRequest."""
+    self.request_id = request_id if request_id else uuid.uuid4().hex
+    self.recipe = recipe
+    self.context = context if context else {}
+    self.evidence = evidence_ if evidence_ else []
+    self.type = self.__class__.__name__
 
-  def _client_setup(self):
-    """Creates an API client for talking to Google Cloud.
+  def to_json(self):
+    """Convert object to JSON.
 
     Returns:
-        A client object for interacting with the cloud service.
+      A JSON serialized object.
     """
-    log.info(u'Creating API client for service: {0:s}'.format(self.service))
-    credentials = oauth2client.GoogleCredentials.get_application_default()
-    http = httplib2.Http()
-    credentials.authorize(http)
-    self.client = discovery.build(self.service, 'v1', http=http)
-    return self.client
+    serializable = copy.deepcopy(self.__dict__)
+    serializable['evidence'] = [x.serialize() for x in serializable['evidence']]
 
+    try:
+      serialized = json.dumps(serializable)
+    except TypeError as e:
+      msg = ('JSON serialization of TurbiniaRequest object {0:s} failed: '
+             '{1:s}'.format(self.type, str(e)))
+      raise TurbiniaException(msg)
 
-class PubSubMessage(dict):
+    return serialized
 
-  def __init__(self, message_type):
-    self['message_type'] = message_type
-    super(PubSubMessage, self).__init__()
-
-
-class PubSubClient(GoogleCloudClient):
-
-  def __init__(self, topic):
-    self.topic = topic
-    super(PubSubClient, self).__init__(u'pubsub')
-    config.LoadConfig()
-    self.subscription = u'projects/{0:s}/subscriptions/{1:s}'.format(
-        config.PROJECT, self.topic)
-
-  def _setup(self):
-    self._client_setup()
-
-  def _validate_message(self, message):
-    """Validates pubsub messages to ensure required fields are available.
+  def from_json(self, json_str):
+    """Loads JSON serialized data into self.
 
     Args:
-      message: dict of pubsub message
+      json_str (str): Json serialized TurbiniaRequest object.
+
+    Raises:
+      TurbiniaException: If json can not be loaded, or deserialized object is
+          not of the correct type.
+    """
+    try:
+      obj = json.loads(json_str)
+    except ValueError as e:
+      raise TurbiniaException(
+          u'Can not load json from string {0:s}'.format(str(e)))
+
+    if obj.get('type', None) != self.type:
+      raise TurbiniaException(
+          u'Deserialized object does not have type of {0:s}'.format(self.type))
+
+    obj['evidence'] = [evidence.evidence_decode(e) for e in obj['evidence']]
+    # pylint: disable=attribute-defined-outside-init
+    self.__dict__ = obj
+
+
+class TurbiniaPubSub(object):
+  """PubSub client object for Google Cloud.
+
+  Attributes:
+    topic: The pubsub topic object
+    topic_name: The pubsub topic name
+    subscription: The pubsub subscription object
+  """
+
+  def __init__(self, topic_name):
+    """Initialization for PubSubClient."""
+    self.topic_name = topic_name
+    self.topic = None
+    self.subscription = None
+
+  def setup(self):
+    """Set up the client."""
+    config.LoadConfig()
+    client = pubsub.Client(project=config.PROJECT)
+    self.topic = client.topic(self.topic_name)
+    log.debug('Connecting to PubSub Subscription on {0:s}'.format(self.topic))
+    self.subscription = self.topic.subscription(self.topic_name)
+
+  def _validate_message(self, message):
+    """Validates pubsub messages and returns them as a new TurbiniaRequest obj.
+
+    Args:
+      message: PubSub message string
 
     Returns:
-      Bool indicating whether message is properly validated
+      A TurbiniaRequest object or None if there are decoding failures.
     """
-    required_fields = {
-        ARTIFACTNEW: [u'artifact'],
-        TASKUPDATE: [u'task_id', u'job_id', u'status'],
-        WORKERUPDATE: [u'worker_id', u'status'],
-        TASKSTART: [u'task_id', u'job_id'],
-        TASKSTOP: [u'task_id', u'job_id'],}
+    request = TurbiniaRequest()
+    try:
+      request.from_json(message)
+    except TurbiniaException as e:
+      log.error(u'Error decoding pubsub message: {0:s}'.format(str(e)))
+      return None
 
-    # TODO(aarontp): Enforce these
-    optional_fields = {
-        ARTIFACTNEW: [],
-        TASKUPDATE: [u'update_text', u'result'],
-        WORKERUPDATE: [u'update_text', u'result'],
-        TASKSTART: [],
-        TASKSTOP: [],}
+    return request
 
-    if not message.has_key(u'message_type'):
-      log.error(u'Message has no message_type: {0:s}'.format(str(message)))
-      return False
-
-    for field in required_fields.get(message[u'message_type'], []):
-      if not message.has_key(field):
-        log.error(
-            u'Message type {0:s} must have field {1:s}: {2:s}'.format(
-                message.get(u'message_type'), field, str(message)))
-        return False
-
-    return True
-
-  def check_message(self):
+  def check_messages(self):
     """Checks for a pubsub message.
 
     Returns:
-      Data dict if message is received, else None
+      A list of any TurbiniaRequest objects received, else an empty list
     """
-    data = None
-    body = {
-        u'returnImmediately': False,
-        u'maxMessages': 1,}
+    results = self.subscription.pull(return_immediately=True)
+    logging.info(u'Recieved {0:d} pubsub messages'.format(len(results)))
 
-    resp = self.client.projects().subscriptions().pull(
-        subscription=self.subscription, body=body).execute()
-    received_messages = resp.get(u'receivedMessages')
-    if received_messages is not None:
-      ack_ids = []
-      received_message = received_messages[0]
-      pubsub_message = received_message.get(u'message')
-      if pubsub_message:
-        log.info(u'PubSub message received')
-        # Process messages
-        data = base64.b64decode(str(pubsub_message.get(u'data')))
-        try:
-          data = json.loads(data)
-          log.info(u'Message body: {0:s}'.format(data))
-        except (ValueError, KeyError) as e:
-          log.error(u'Error processing message: {0:s}'.format(e))
+    ack_ids = []
+    requests = []
+    for ack_id, message in results:
+      data = message.data
+      log.info(u'Processing PubSub Message {0:s}'.format(message.message_id))
+      log.debug(u'PubSub Message body: {0:s}'.format(data))
 
-        # Get the message's ack ID
-        ack_ids.append(received_message.get(u'ackId'))
+      request = self._validate_message(data)
+      if request:
+        requests.append(request)
+        ack_ids.append(ack_id)
+      else:
+        log.error(u'Error processing PubSub message: {0:s}'.format(data))
 
-        # Create a POST body for the acknowledge request
-        ack_body = {u'ackIds': ack_ids}
+    if results:
+      self.subscription.acknowledge(ack_ids)
 
-        # Acknowledge the message.
-        self.client.projects().subscriptions().acknowledge(
-            subscription=self.subscription, body=ack_body).execute()
+    return requests
 
-    if not self._validate_message(data):
-      log.error('Error processing invalid message: {0:s}'.format(data))
-
-    return data
-
-  # TODO(aarontp): fill in
   def send_message(self, message):
-    pass
+    """Send a pubsub message.
+
+    message: The message to send.
+    """
+    data = message.encode('utf-8')
+    msg_id = self.topic.publish(data)
+    logging.info(u'Published message {0:s} to topic {1:s}'.format(
+        msg_id, self.topic_name))
+
+  def send_request(self, request):
+    """Sends a TurbiniaRequest message.
+
+    Args:
+      request: A TurbiniaRequest object.
+    """
+    self.send_message(request.to_json())
