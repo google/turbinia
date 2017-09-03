dcb476a9344c3d2ebadf84af1d9c36c679fca15f
Aaron Peterson <aaronp@gmail.com>
2017-09-03 13:21:45 -0700

    Fix method path...

--- turbinia/evidence.py
+++ turbinia/evidence.py
@@ -244,7 +244,7 @@ class GoogleCloudDiskRawEmbedded(GoogleCloudDisk):
 
   def postprocess(self):
     google_cloud.PostprocessDetachDisk(self)
-    mount_local.PreprocessUnmountDisk(self)
+    mount_local.PostprocessUnmountDisk(self)
 
 
 class PlasoFile(Evidence):
