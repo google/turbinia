5fdf4a2a238b89320fabdbbb4109838574d0f3fe
Aaron Peterson <aaronp@gmail.com>
2017-11-21 15:34:08 -0800

    Fix up the loggers and eliminate double logging (#109)

--- turbinia/config/logger.py
+++ turbinia/config/logger.py
@@ -18,36 +18,54 @@ import logging
 from turbinia import config
 
 
-def setup(root=False):
+def setup():
   """Set up logging parameters.
 
-  By default this will not set the root logger, which is the default logger when
-  a named logger is not specified.  We currently use 'turbinia' as the named
-  logger, however some external modules that are called by Turbinia can use the
-  root logger, so we want to be able to optionally configure that as well.
-
-  Args:
-    root: Boolean indicating whether root logger should also be configured.
+  This will also set the root logger, which is the default logger when a named
+  logger is not specified.  We currently use 'turbinia' as the named logger,
+  however some external modules that are called by Turbinia can use the root
+  logger, so we want to be able to optionally configure that as well.
   """
   # TODO(aarontp): Add a config option to set the log level
   config.LoadConfig()
-  log = logging.getLogger('turbinia')
+  logger = logging.getLogger('turbinia')
+  # Eliminate double logging from root logger
+  logger.propagate = False
+  need_file_handler = True
+  need_stream_handler = True
+
+  # We only need a handler if one of that type doesn't exist already
+  if logger.handlers:
+    for handler in logger.handlers:
+      # Want to do strict type-checking here because is instance will include
+      # subclasses and so won't distinguish between StreamHandlers and
+      # FileHandlers.
+      # pylint: disable=unidiomatic-typecheck
+      if type(handler) == logging.FileHandler:
+        need_file_handler = False
+
+      # pylint: disable=unidiomatic-typecheck
+      if type(handler) == logging.StreamHandler:
+        need_stream_handler = False
 
   file_handler = logging.FileHandler(config.LOG_FILE)
   formatter = logging.Formatter(u'%(asctime)s:%(levelname)s:%(message)s')
   file_handler.setFormatter(formatter)
   file_handler.setLevel(logging.DEBUG)
+  if need_file_handler:
+    logger.addHandler(file_handler)
 
   console_handler = logging.StreamHandler()
   formatter = logging.Formatter(u'[%(levelname)s] %(message)s')
   console_handler.setFormatter(formatter)
+  if need_stream_handler:
+    logger.addHandler(console_handler)
 
-  log.addHandler(file_handler)
-  log.addHandler(console_handler)
-
-  # Optionally configure the root logger because other modules like PSQ use
-  # this, and we want to see log messages from it when executing from CLI.
-  if root:
-    root_log = logging.getLogger()
-    root_log.addHandler(console_handler)
-    root_log.setLevel(logging.DEBUG)
+  # Configure the root logger to use exactly our handlers because other modules
+  # like PSQ use this, and we want to see log messages from it when executing
+  # from CLI.
+  root_log = logging.getLogger()
+  for handler in root_log.handlers:
+    root_log.removeHandler(handler)
+  root_log.addHandler(console_handler)
+  root_log.addHandler(file_handler)
