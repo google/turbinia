28926db7120fbf879c02e5a826d3119f9e0894f5
Aaron Peterson <aaronp@gmail.com>
2017-12-08 12:56:19 -0800

    Add Checks to avoid requests to incompatible instances

--- turbinia/evidence.py
+++ turbinia/evidence.py
@@ -66,6 +66,11 @@ class Evidence(object):
   evidence.
 
   Attributes:
+    cloud_only: Set to True for evidence types that can only be processed in a
+        cloud environment, e.g. GoogleCloudDisk.
+    copyable: Whether this evidence can be copied.  This will be set to True for
+        object types that we want to copy to/from storage (e.g. PlasoFile, but
+        not RawDisk).
     name: Name of evidence.
     description: Description of evidence.
     source: String indicating where evidence came from (including tool version
@@ -84,6 +89,8 @@ class Evidence(object):
       tags=None,
       request_id=None):
     """Initialization for Evidence."""
+    self.copyable = False
+    self.cloud_only = False
     self.description = description
     self.source = source
     self.local_path = local_path
@@ -209,6 +216,7 @@ class GoogleCloudDisk(RawDisk):
     self.disk_name = disk_name
     self.type = type_
     super(GoogleCloudDisk, self).__init__(*args, **kwargs)
+    self.cloud_only = True
 
   def preprocess(self):
     google_cloud.PreprocessAttachDisk(self)
@@ -255,6 +263,7 @@ class PlasoFile(Evidence):
     """Initialization for Plaso File evidence."""
     self.plaso_version = plaso_version
     super(PlasoFile, self).__init__(*args, **kwargs)
+    self.copyable = True
 
 
 class PlasoCsvFile(PlasoFile):
@@ -273,3 +282,4 @@ class ReportText(Evidence):
   def __init__(self, text_data=None, *args, **kwargs):
     self.text_data = text_data
     super(ReportText, self).__init__(*args, **kwargs)
+    self.copyable = True
