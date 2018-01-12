6d781c0a0abe34be236bad35f341d60811e5d690
Aaron Peterson <aaronp@gmail.com>
2018-01-11 17:04:16 -0800

    update config text

--- turbinia/config/turbinia_config.py
+++ turbinia/config/turbinia_config.py
@@ -45,9 +45,10 @@ MOUNT_DIR_PREFIX = '/mnt/turbinia-mounts'
 # NFS or a SAN for Evidence objects.
 SHARED_FILESYSTEM = False
 
-# This will set debugging flags and file output on processes executed by Tasks.
-# This could cause performance issues with some tasks, so it is recommended to
-# only set this to True when debugging problems.
+# This will set debugging flags for processes executed by Tasks (for
+# Tasks/binaries that support it).  This could cause performance issues with
+# some tasks, so it is recommended to only set this to True when debugging
+# problems.
 DEBUG_TASKS = False
 
 # GCE configuration
