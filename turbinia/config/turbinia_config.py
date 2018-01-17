d0690178b0335c665821e57648499403d0e21df2
beamcodeup <tree@google.com>
2018-01-16 18:00:51 -0800

    tweak take 7

--- turbinia/config/turbinia_config.py
+++ turbinia/config/turbinia_config.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Google Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -58,7 +58,7 @@ OUTPUT_DIR = '%s/output' % GCS_MOUNT_DIR
 # Time in seconds to sleep in task management loops
 SLEEP_TIME = 10
 
-# Whether to run as a single run, or to keep server running indefinitely
+# True if running as a single run, or False if keeping server running indefinitely
 SINGLE_RUN = False
 
 # Local directory in the worker to put other mount directories for locally
@@ -66,7 +66,7 @@ SINGLE_RUN = False
 MOUNT_DIR_PREFIX = '/mnt/turbinia-mounts'
 
 # This indicates whether the workers are running in an environment with a shared
-# filesystem.  This should be False for environments with workers running in
+# filesystem. This should be False for environments with workers running in
 # GCE, and True for environments that have workers on dedicated machines with
 # NFS or a SAN for Evidence objects.
 SHARED_FILESYSTEM = False
@@ -81,14 +81,13 @@ DEBUG_TASKS = False
 ###############
 # GCP AND GCE #
 ###############
-
 PROJECT = None
 ZONE = None
 INSTANCE = None
 DEVICE_NAME = None
 SCRATCH_PATH = None
 # GCS bucket that has Turbinia-specific scripts and can be used to store logs.
-BUCKET_NAME = None
+BUCKET_NAME = 'turbinia'
 PSQ_TOPIC = 'turbinia-psq'
 
 # Topic Turbinia will listen on for new Artifact events. This is also used as
