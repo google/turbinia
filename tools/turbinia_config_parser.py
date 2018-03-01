a67f42e1488128b2395b6117f2f0af9771055e69
beamcodeup <tree@google.com>
2018-03-01 15:40:01 -0800

    Minor fix for the config parser.

--- tools/turbinia_config_parser.py
+++ tools/turbinia_config_parser.py
@@ -13,37 +13,46 @@
 # # See the License for the specific language governing permissions and
 # # limitations under the License.
 
+
+"""Turbinia Config Parser."""
+
 from __future__ import unicode_literals
 import imp
 import itertools
 import os
 import sys
 
-# This Turbinia config parser is written for init scripts using systemd,
-# so this will not look for config files anywhere, except for /etc/turbinia.
-# If there's a need to run Turbinia based on the config file in a homedir,
-# you will need to run manually the start scripts.
+# This Turbinia config parser can be invoked by any scripts to get the config.
+# Note that this will not look for config files anywhere, except for
+# /etc/turbinia. If there's a need to run Turbinia based on the config file in
+# a homedir, you will need to run manually Turbinia.
 
 # Look for config files with these names
 CONFIGFILES = ['turbinia.conf', 'turbinia_config.py']
 CONFIGPATH = ['/etc/turbinia']
 
+
 def main():
+  if len(sys.argv) < 2:
+    print '%s <key name>' % sys.argv[0]
+    sys.exit(1)
   key = sys.argv[1]
   if key:
     config_file = None
-    for _dir, _file in itertools.product(CONFIGPATH, CONFIGFILES):
-      if os.path.exists(os.path.join(_dir, _file)):
-        config_file = os.path.join(_dir, _file)
+    for dirname, filename in itertools.product(CONFIGPATH, CONFIGFILES):
+      if os.path.exists(os.path.join(dirname, filename)):
+        config_file = os.path.join(dirname, filename)
         break
     if config_file is None:
-      exit(1)
+      sys.exit(2)
     config = imp.load_source('config', config_file)
-    import config
+    import config  # pylint: disable=g-import-not-at-top
     try:
       print getattr(config, key.upper())
-    except:
-      exit(2)
+    except AttributeError:
+      print 'Key not found: %s' % key
+      sys.exit(3)
+
 
 if __name__ == '__main__':
   main()
