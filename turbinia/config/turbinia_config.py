ac14e9adc28dee613ca3c610efd7eb5b9d4ecc2f
beamcodeup <547397+beamcodeup@users.noreply.github.com>
2018-04-10 11:58:22 -0700

    Clean up and clarify config file (#164)
    * clean up and clarify config file
    
    * add 1 more line
    
    * More cleaning up
    
    * add ROLE var
    
    * Clean up
    
    * correct the worker type as psqworker
    
    * add more variables
    
    * Fix the gcs mount point variable.

--- turbinia/config/turbinia_config.py
+++ turbinia/config/turbinia_config.py
@@ -16,20 +16,46 @@
 from __future__ import unicode_literals
 
 
-"""Dummy Turbinia config file."""
+"""Turbinia Config Template"""
 
-# Turbinia Config
+############
+# TURBINIA #
+############
 
-# 'PSQ' is currently the only valid option
+# Turbinia Role as 'server' or 'psqworker'
+ROLE = 'server'
+
+# Which user account Turbinia runs as
+USER = 'turbinia'
+
+# Turbinia's installion directory
+TURBINIA_DIR = '/opt/turbinia'
+
+# GCSFuse mount for Turbinia scripts and logging.
+# Note: this GCS instance is where Google Cloud Functions should
+# be deployed to.
+GCS_MOUNT_DIR = '/mnt/turbinia'
+
+# Virtualenv directory
+TURBINIAENV = '%s/turbinia-env' % HOME_DIR
+
+# Local directory for temporary data
+TMP_DIR = '/var/tmp'
+
+# 'PSQ' is currently the only valid option as
+# a distributed task queue using Google Cloud Pub/Sub
 TASK_MANAGER = 'PSQ'
 
-# File to log to
-LOG_FILE = None
+# File to log to; set this as None if log file is not desired
+# By default, Turbinia logs are written to a directory (GCS_MOUNT_DIR) in the GCS mount
+LOG_FILE = '%s/output/logs/turbinia.log' % GCS_MOUNT_DIR
 
-# Default output directory
-OUTPUT_DIR = None
+# Default base output directory for worker results and evidence
+# When running Turbinia locally, you can set this to, for example,
+# %/turbinia_output' % TMP_DIR
+OUTPUT_DIR = '%s/output' % GCS_MOUNT_DIR
 
-# Time to sleep in task management loops
+# Time in seconds to sleep in task management loops
 SLEEP_TIME = 10
 
 # Whether to run as a single run, or to keep server running indefinitely
@@ -51,22 +77,33 @@ SHARED_FILESYSTEM = False
 # problems.
 DEBUG_TASKS = False
 
-# GCE configuration
+
+###############
+# GCP AND GCE #
+###############
+
 PROJECT = None
 ZONE = None
 INSTANCE = None
 DEVICE_NAME = None
 SCRATCH_PATH = None
+# GCS bucket that has Turbinia-specific scripts and can be used to store logs.
 BUCKET_NAME = None
 PSQ_TOPIC = 'turbinia-psq'
 
-# Topic Turbinia will listen on for new Artifact events.  This is also used as
+# Topic Turbinia will listen on for new Artifact events. This is also used as
 # the Turbinia instance/namespace as it is a unique string per Turbinia
 # instance and Cloud Project.
-PUBSUB_TOPIC = None
+PUBSUB_TOPIC = 'turbinia-pubsub'
 
 # GCS Path to copy worker results and Evidence output to
-GCS_OUTPUT_PATH = False
+# Otherwise, set this as None if output will be stored locally.
+GCS_OUTPUT_PATH = 'gs://%s/output' % BUCKET_NAME
 
 # Which state manager to use
 STATE_MANAGER = 'Datastore'
+
+REDIS_HOST = 'None'
+REDIS_PORT = 'None'
+TIMESKETCH_HOST = 'None'
+TIMESKETCH_USER = 'None'
