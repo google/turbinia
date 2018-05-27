b2aaba27f0af620413604612911931d45175b5b7
Eric Zinnikas <ericwz@fb.com>
2018-05-26 22:53:38 -0400

    Handle deprecated Pip commands

--- setup.py
+++ setup.py
@@ -23,8 +23,12 @@ sudo python setup.py install
 
 from setuptools import find_packages
 from setuptools import setup
-from pip.req import parse_requirements
-from pip.download import PipSession
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    from pip.download import PipSession
 
 setup(
     name='Turbinia',
