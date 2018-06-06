84e0e64816571f6b6b33e3a622011f323f47dfda
Johan Berggren <jberggren@gmail.com>
2018-06-06 15:07:41 +0200

    Add support for pip 10 and add worker install target

--- setup.py
+++ setup.py
@@ -23,8 +23,13 @@ sudo python setup.py install
 
 from setuptools import find_packages
 from setuptools import setup
-from pip.req import parse_requirements
-from pip.download import PipSession
+
+try: # for pip >= 10
+  from pip._internal.download import PipSession
+  from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+  from pip.download import PipSession
+  from pip.req import parse_requirements
 
 setup(
     name='Turbinia',
@@ -36,4 +41,7 @@ setup(
     scripts=['turbiniactl'],
     install_requires=[str(req.req) for req in parse_requirements(
         'requirements.txt', session=PipSession())
-    ])
+    ],
+    extras_require = {
+      'worker': ['plaso>=20171118'],
+    })
