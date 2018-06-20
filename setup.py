60676c8f5e9bb563d7ac61b6288cac1d8cd2122b
Johan Berggren <jberggren@gmail.com>
2018-06-20 08:09:07 +0200

    Install Plaso only for workers (#183)
    * Add support for pip 10 and add worker install target
    
    * Worker extras
    
    * More setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#!/usr/bin/python
 #
 # Copyright 2017 Google Inc.
 #
@@ -14,26 +14,54 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This is the setup file for the project.
+"""This is the setup file for the project."""
 
-The standard setup rules apply:
-python setup.py build
-sudo python setup.py install
-"""
+from __future__ import unicode_literals
 
 from setuptools import find_packages
 from setuptools import setup
-from pip.req import parse_requirements
-from pip.download import PipSession
+
+try:  # for pip >= 10
+  from pip._internal.download import PipSession
+  from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+  from pip.download import PipSession
+  from pip.req import parse_requirements
+
+
+turbinia_version = '20180620'
+
+turbinia_description = (
+  'Turbinia is an open-source framework for deploying, managing, and running'
+  'forensic workloads on cloud platforms. It is intended to automate running of'
+  'common forensic processing tools (i.e. Plaso, TSK, strings, etc) to help'
+  'with processing evidence in the Cloud, scaling the processing of large'
+  'amounts of evidence, and decreasing response time by parallelizing'
+  'processing where possible.')
 
 setup(
-    name='Turbinia',
-    version='20170801',
-    long_description=__doc__,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={"console_scripts": ["turbiniactl=turbinia.turbiniactl:main"]},
-    install_requires=[str(req.req) for req in parse_requirements(
-        'requirements.txt', session=PipSession())
-    ])
+  name='turbinia',
+  version=turbinia_version,
+  description='Automation and Scaling of Digital Forensics Tools',
+  long_description=turbinia_description,
+  license='Apache License, Version 2.0',
+  url='http://turbinia.plumbing/',
+  maintainer='Turbinia development team',
+  maintainer_email='turbinia-dev@googlegroups.com',
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+  ],
+  packages=find_packages(),
+  include_package_data=True,
+  zip_safe=False,
+  entry_points = {"console_scripts": ["turbiniactl=turbinia.turbiniactl:main"]},
+  install_requires=[str(req.req) for req in parse_requirements(
+    'requirements.txt', session=PipSession())
+  ],
+  extras_require={
+    'worker': ['plaso>=20171118']
+  }
+)
