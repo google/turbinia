df4953f07c4ad532158c1ed8cfdc2a2898b7262f
Aaron Peterson <aaronp@gmail.com>
2018-09-18 06:21:24 -0700

    Change wordpress task so tests run deterministically

--- turbinia/workers/analysis/wordpress_test.py
+++ turbinia/workers/analysis/wordpress_test.py
@@ -27,7 +27,7 @@ class WordpressAccessLogAnalysisTaskTest(unittest.TestCase):
   """Tests for WordpressAccessLogAnalysis Task."""
 
   WORDPRESS_ACCESS_LOGS = None
-  WORDPRESS_PWNED_REPORT = """Wordpress access logs found (theme_edit, install)
+  WORDPRESS_PWNED_REPORT = """Wordpress access logs found (install, theme_edit)
 \t27/Jun/2018:19:29:54 +0000: Wordpress installation successful
 \t27/Jun/2018:19:31:15 +0000: Wordpress theme editor edited file (header.php)
 """ # pylint: disable=line-too-long
