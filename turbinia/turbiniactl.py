59b557b92a8e4e47b400685f7dfbbf369ff51943
CiaranFahy <34317848+CiaranFahy@users.noreply.github.com>
2019-05-24 17:46:58 +0100

    Thanks ericzinnikas

--- turbinia/turbiniactl.py
+++ turbinia/turbiniactl.py
@@ -276,7 +276,7 @@ def main():
   args = parser.parse_args()
   if args.config:
     try:
-      with open(config.configSource(), "r") as f:
+      with open(config.configSource, "r") as f:
         conf = f.read()
         print(conf)
     except:
