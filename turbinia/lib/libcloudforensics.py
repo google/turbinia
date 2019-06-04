e9003e6401dc8f3a72d796bb293860d51efcb879
Thomas Chopitea <tomchop@gmail.com>
2019-06-04 12:44:55 +0200

    Make sure crc32 uses bytes, reintroduce sleuthkit (#396)
    * Make sure crc32 uses bytes, reintroduce sleuthkit
    
    * Fix indentation

--- turbinia/lib/libcloudforensics.py
+++ turbinia/lib/libcloudforensics.py
@@ -270,7 +270,8 @@ class GoogleCloudProject(object):
     # Max length of disk names in GCP is 63 characters
     project_id = snapshot.project.project_id
     disk_id = project_id + snapshot.disk.name
-    disk_id_crc32 = '{0:08x}'.format(binascii.crc32(disk_id) & 0xffffffff)
+    disk_id_crc32 = '{0:08x}'.format(
+        binascii.crc32(disk_id.encode()) & 0xffffffff)
     truncate_at = 62 - len(disk_id_crc32) - len('-copy') - len(project_id)
     if disk_name_prefix:
       disk_name_prefix += '-'
@@ -330,7 +331,7 @@ class GoogleCloudProject(object):
 
     # Analysis software to install.
     packages_to_install = [
-        'python-plaso', 'xmount', 'libfvde-tools', 'libbde-tools',
+        'python-plaso', 'xmount', 'sleuthkit', 'libfvde-tools', 'libbde-tools',
         'plaso-tools', 'jq', 'ncdu', 'htop', 'binutils', 'upx-ucl'
     ]
 
