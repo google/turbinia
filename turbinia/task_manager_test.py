38c5da4f95f340a5083e0e20c9835fc29bfe9683
Aaron Peterson <aaronp@gmail.com>
2019-08-30 17:11:04 -0700

    Add test file and new Task

---
+++ turbinia/task_manager_test.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for Turbinia task_manager module."""
+
+from __future__ import unicode_literals
+
+import mock
+
+from turbinia import task_manager
+from turbinia.jobs import manager as jobs_manager
+from turbinia.jobs import plaso
+from turbinia.jobs import strings
+from turbinia.workers.workers_test import TestTurbiniaTaskBase
+
+
+class TestTaskManager(TestTurbiniaTaskBase):
+  """Tests the task_manager module."""
+
+  @mock.patch('turbinia.task_manager.state_manager.get_state_manager')
+  def setUp(self, _):
+    """Sets up the test class."""
+    super(TestTaskManager, self).setUp()
+    self.manager = task_manager.BaseTaskManager()
+    self.job1 = plaso.PlasoJob()
+    self.job2 = strings.StringsJob()
+
+  def testTaskManagerTasksProperty(self):
+    """Basic test for task_manager Tasks property."""
+    self.setResults()
+    job = jobs_manager.JobsManager.GetJobInstance('PlasoJob')
+    job.tasks.extend([self.task, self.task])
+    self.manager.running_jobs.extend([job, job])
+    self.assertEqual(len(self.manager.tasks), 4)
+
+  def testAddEvidence(self):
+    """Tests add_evidence method."""
+    self.setResults()
+    request_id = 'testRequestID'
+    self.evidence.request_id = request_id
+    self.manager.add_task = mock.MagicMock()
+    job = plaso.PlasoJob
+    job.create_tasks = mock.MagicMock(return_value=[self.task])
+    self.manager.jobs = [job]
+    self.manager.add_evidence(self.evidence)
+
+    self.manager.add_task.assert_called()
+    test_job = self.manager.running_jobs[0]
+    test_job.create_tasks.assert_called()
+    self.assertEqual(test_job.request_id, request_id)
+    self.assertEqual(test_job.evidence.request_id, request_id)
+    self.assertIn(test_job, self.manager.running_jobs)
+
+  def testAddEvidenceBlackList(self):
+    """Tests add_evidence method."""
+    self.setResults()
+    self.manager.add_task = mock.MagicMock()
+    self.job1 = plaso.PlasoJob
+    self.job2 = strings.StringsJob
+    self.job1.create_tasks = mock.MagicMock(return_value=[self.task])
+    self.job2.create_tasks = mock.MagicMock(return_value=[self.task])
+    self.manager.jobs = [self.job1, self.job2]
+    self.evidence.config['jobs_blacklist'] = ['StringsJob']
+    self.manager.add_evidence(self.evidence)
+
+    # Only one Plaso job is queued after one is blacklisted
+    self.assertEqual(len(self.manager.running_jobs), 1)
+    test_job = self.manager.running_jobs[0]
+    self.assertEqual(test_job.name, 'PlasoJob')
+
+  def testAddEvidenceWhitelist(self):
+    """Tests add_evidence method."""
+    self.setResults()
+    self.manager.add_task = mock.MagicMock()
+    self.job1 = plaso.PlasoJob
+    self.job2 = strings.StringsJob
+    self.job1.create_tasks = mock.MagicMock(return_value=[self.task])
+    self.job2.create_tasks = mock.MagicMock(return_value=[self.task])
+    self.manager.jobs = [self.job1, self.job2]
+    self.evidence.config['jobs_whitelist'] = ['PlasoJob']
+    self.manager.add_evidence(self.evidence)
+
+    # Only one Plaso job is queued after one is blacklisted
+    self.assertEqual(len(self.manager.running_jobs), 1)
+    test_job = self.manager.running_jobs[0]
+    self.assertEqual(test_job.name, 'PlasoJob')
+
+  def testCheckRequestDoneIsDone(self):
+    """Basic test for check_request_done for when the request is done."""
+    request_id = 'testId'
+    self.job1.request_id = request_id
+    self.job2.request_id = request_id
+    # We want evidence associated with each Job, but no Tasks so that it thinks
+    # they are completed.
+    self.job1.evidence.collection.append(self.evidence)
+    self.job2.evidence.collection.append(self.evidence)
+    self.manager.running_jobs = [self.job1, self.job2]
+    self.assertTrue(self.manager.check_request_done(request_id))
+
+  def testCheckRequestDoneIsNotDone(self):
+    """Basic test for check_request_done for when the request is done."""
+    request_id = 'testId'
+    self.job1.request_id = request_id
+    self.job2.request_id = request_id
+    self.manager.running_jobs = [self.job1, self.job2]
+    # With no associated evidence the Jobs will show as not yet done.
+    self.assertFalse(self.manager.check_request_done(request_id))
+
+  def testGetJob(self):
+    """Tests get_job method."""
+    self.setResults()
+    job_id = 'testID'
+    self.job1.id = job_id
+    self.job2.id = 'NotMyJob'
+    self.manager.running_jobs = [self.job1, self.job2]
+    test_job = self.manager.get_job(job_id)
+    self.assertEqual(test_job.name, 'PlasoJob')
+    self.assertEqual(test_job.id, job_id)
+
+  def testAddTask(self):
+    """Tests add_task method."""
+    request_id = 'testID'
+    self.evidence.request_id = request_id
+    self.manager.enqueue_task = mock.MagicMock()
+    self.manager.add_task(self.task, self.job1, self.evidence)
+    self.assertEqual(self.task.request_id, request_id)
+    self.assertListEqual(self.job1.tasks, [self.task])
+    self.manager.enqueue_task.assert_called()
+
+  def testRemoveJob(self):
+    """Tests remove_job method."""
+    job_id = 'testID'
+    self.job1.id = job_id
+    self.manager.running_jobs.extend([self.job1, self.job2])
+    self.assertTrue(self.manager.remove_job(job_id))
+    self.assertListEqual(self.manager.running_jobs, [self.job2])
+
+  def testFinalizeResult(self):
+    """Tests finalize_result method."""
+    job_id = 'testJobID'
+    self.job1.id = job_id
+    self.result.job_id = job_id
+    self.result.evidence.append(self.evidence)
+    self.manager.add_evidence = mock.MagicMock()
+    self.manager.running_jobs.append(self.job1)
+    test_job = self.manager.finalize_result(self.result)
+    self.assertEqual(test_job.id, job_id)
+    self.assertEqual(test_job, self.manager.running_jobs[0])
+    self.assertEqual(test_job.evidence.collection[0], self.evidence)
+    self.manager.add_evidence.assert_called_with(self.evidence)
+
+  def testFinalizeResultBadEvidence(self):
+    """Tests finalize_result method with bad input evidence."""
+    job_id = 'testJobID'
+    self.job1.id = job_id
+    self.result.job_id = job_id
+    self.result.evidence = None
+    self.assertIsNone(self.manager.finalize_result(self.result))
+    self.assertIsInstance(self.result.evidence, list)
+
+  def testFinalizeJob(self):
+    """Tests finalize_job method."""
+    task_id = 'testTaskID'
+    self.task.id = task_id
+    self.job1.request_id = 'testRequestID'
+    self.job1.create_final_tasks = mock.MagicMock()
+    self.job1.evidence.add_evidence(self.evidence)
+    self.manager.add_task = mock.MagicMock()
+    self.job1.tasks.append(self.task)
+    self.manager.running_jobs.append(self.job1)
+    self.manager.finalize_job(self.job1, task_id)
+
+    self.job1.create_final_tasks.assert_called()
+    self.manager.add_task.assert_called()
+    _, __, test_evidence = self.manager.add_task.call_args[0]
+    self.assertListEqual(test_evidence.collection, [self.evidence])
+
+  def testRun(self):
+    """Test the run() method."""
+    result_id = 'testResultID'
+    self.manager.get_evidence = mock.MagicMock(return_value=[self.evidence])
+    self.manager.add_evidence = mock.MagicMock()
+    self.result.id = result_id
+    self.task.result = self.result
+    self.manager.process_tasks = mock.MagicMock(return_value=[self.task])
+    self.manager.finalize_result = mock.MagicMock(return_value=self.job1)
+    self.manager.finalize_job = mock.MagicMock()
+    self.manager.run(under_test=True)
+
+    self.manager.add_evidence.assert_called_with(self.evidence)
+    self.manager.finalize_result.assert_called_with(self.result)
+    self.manager.finalize_job.assert_called_with(self.job1, result_id)
---
+++ turbinia/workers/finalize_request.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Task to finalize the request."""
+
+from __future__ import unicode_literals
+
+import os
+
+from turbinia import config
+from turbinia.evidence import FinalReport
+from turbinia.workers import TurbiniaTask
+
+
+class RequestFinalizeTask(TurbiniaTask):
+  """Task to finalize the Turbinia request."""
+
+  def run(self, evidence, result):
+    """Main entry point for Task.
+
+    This generates a final report.
+
+    Args:
+        evidence (EvidenceCollection): All Evidence that has been generated as
+            part of this request.
+        result (TurbiniaTaskResult): The result to place task output into.
+
+    Returns:
+        TurbiniaTaskResult object.
+    """
+    # Doing a delayed import to avoid circular dependencies.
+    from turbinia.client import TurbiniaClient
+    report = FinalReport()
+    client = TurbiniaClient()
+
+    report_file = os.path.join(
+        self.tmp_dir, 'final_turbinia_report_{0:s}.md'.format(self.id))
+    report.local_path = report_file
+    report_data = client.format_task_status(
+        config.INSTANCE_ID, config.TURBINIA_PROJECT, config.TURBINIA_REGION,
+        request_id=evidence.request_id, full_report=True)
+
+    result.log('Writing report data to [{0:s}]'.format(report.local_path))
+    with open(report.local_path, 'wb') as file_handle:
+      file_handle.write(report_data.encode('utf-8'))
+
+    result.close(self, True)
+    return result
