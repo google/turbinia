e0678aeb89867cf8c7130849536307bd42985e52
CiaranFahy <ciaran@ciaranfahy.com>
2019-11-02 20:32:00 +0000

    added docstring to top

--- turbinia/notify.py
+++ turbinia/notify.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+'''Sends email notifications'''
+
 import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
