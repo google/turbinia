b43cadee6b0fe259653e7e6408b444d43530a631
CiaranFahy <ciaran@ciaranfahy.com>
2019-11-06 11:42:00 +0000

    formatting

--- turbinia/notify.py
+++ turbinia/notify.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Sends email notifications"""
 
 import logging
@@ -20,6 +19,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from turbinia import config
+
 log = logging.getLogger('turbinia')
 
 #Sends notfications via email
