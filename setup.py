38447974d9392b45384a53a21de0efc78a408195
Said Eid <eids@google.com>
2019-11-13 16:06:20 +0100

    adding celery and coverage modules to the dev setup

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'requirements.txt', session=PipSession())
     ],
     extras_require={
-        'dev': ['mock', 'nose', 'yapf'],
+        'dev': ['mock', 'nose', 'yapf', 'celery~=4.1', 'coverage'],
         'local': ['celery~=4.1', 'kombu~=4.1', 'redis~=3.0'],
         'worker': ['plaso>=20171118', 'pyhindsight>=2.2.0']
     }
