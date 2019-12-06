28b5887557e87421537999dff4f507c253d33006
Aaron Peterson <aaronp@gmail.com>
2019-12-05 16:47:57 -0800

    Error instead of raise exception

--- turbinia/task_manager.py
+++ turbinia/task_manager.py
@@ -297,18 +297,17 @@ class BaseTaskManager(object):
     Args:
       task: An instantiated Turbinia Task
       evidence_: An Evidence object to be processed.
-
-    Raises:
-      TurbiniaException: If the request_id cannot be found.
     """
     if evidence_.request_id:
       task.request_id = evidence_.request_id
     elif job and job.request_id:
       task.request_id = job.request_id
     else:
-      raise TurbiniaException(
-          'Request ID not found in Evidence {0!s} or Task {1!s}'.format(
+      log.error(
+          'Request ID not found in Evidence {0!s} or Task {1!s}. Not adding '
+          'new Task because of undefined state'.format(
               evidence_, task))
+      return
 
     evidence_.config = job.evidence.config
     task.base_output_dir = config.OUTPUT_DIR
