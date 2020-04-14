e83712c8978e52c338bc98d91c5c77440bb510a0
Wajih Yassine <wajihyassine@gmail.com>
2020-04-14 11:29:03 -0700

    Round 2 fixes

--- turbinia/config/turbinia_config_tmpl.py
+++ turbinia/config/turbinia_config_tmpl.py
@@ -170,7 +170,6 @@ PUBSUB_TOPIC = INSTANCE_ID
 GCS_OUTPUT_PATH = None
 
 # Set this to True if you would like to enable Google Cloud Stackdriver Logging.
-# Please ensure that the Stackdriver Logging API is already enabled.
 STACKDRIVER_LOGGING = False
 
 ################################################################################
