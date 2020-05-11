6b3cad7ff58416557ab8a8b9d9a4f7ca7fe5ae99
Ali Memarzia <amisi@google.com>
2020-05-11 11:55:31 -0700

    change?

--- turbinia/jobs/photorec.py
+++ turbinia/jobs/photorec.py
@@ -25,8 +25,4 @@ class PhotorecJob(interface.TurbiniaJob):
     return [PhotorecTask() for _ in evidence]
 
 
-<<<<<<< HEAD
 manager.JobsManager.RegisterJob(PhotorecJob)
-=======
-manager.JobsManager.RegisterJob(PhotorecJob)
->>>>>>> 0d25e02... resolved conflicts
--- turbinia/workers/photorec.py
+++ turbinia/workers/photorec.py
@@ -28,13 +28,11 @@ class PhotorecTask(TurbiniaTask):
 
   def run(self, evidence, result):
     """Task to execute photorec.
-<<<<<<< HEAD
 
-=======
->>>>>>> 0d25e02... resolved conflicts
     Args:
         evidence (Evidence object):  The evidence we will process.
         result (TurbiniaTaskResult): The object to place task results into.
+
     Returns:
         TurbiniaTaskResult object.
     """
