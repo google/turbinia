083b5ccbfbba2ba95bc76956c1ed28306bb51596
Ali Memarzia <amisi@google.com>
2020-05-14 21:10:42 -0700

    Missed a whiteline

--- turbinia/workers/photorec.py
+++ turbinia/workers/photorec.py
@@ -32,6 +32,7 @@ class PhotorecTask(TurbiniaTask):
     Args:
         evidence (Evidence object):  The evidence we will process.
         result (TurbiniaTaskResult): The object to place task results into.
+
     Returns:
         TurbiniaTaskResult object.
     """
