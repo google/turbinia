d9f9c49175d16b8af4de4a81c4c691a4276d8b66
hacktobeer <ramsesdebeer@gmail.com>
2020-06-23 11:51:26 +0200

    review comments

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "${TURBINIA_CONF}" | base64 -d > /etc/turbinia/turbinia.conf
+
+# Only write the Turbinia configuration file if TURBINIA_CONF is not empty and the file does not exist (or is 0 bytes in size)
+if [ ! -z "$TURBINIA_CONF" ] && [ ! -s /etc/turbinia/turbinia.conf ]
+then
+    echo "${TURBINIA_CONF}" | base64 -d > /etc/turbinia/turbinia.conf
+fi
+
 /usr/local/bin/turbiniactl -L /var/log/turbinia/turbinia.log -S -o /var/lib/turbinia server
