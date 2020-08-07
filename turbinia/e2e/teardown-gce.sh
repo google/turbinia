2a1c16a28895c0f8b18057f044de32060f8f1a02
hacktobeer <ramsesdebeer@gmail.com>
2020-08-07 13:57:27 +0200

    Add teardown items.

--- turbinia/e2e/teardown-gce.sh
+++ turbinia/e2e/teardown-gce.sh
@@ -7,6 +7,19 @@ export DEVSHELL_PROJECT_ID=`gcloud config list --format 'value(core.project)'`
 cd ./forseti-security/contrib/incident-response/infrastructure/
 terraform destroy --target=module.turbinia -var gcp_project=$DEVSHELL_PROJECT_ID -auto-approve
 
-#TODO(rbdebeer) remove evidence disks (all, except the test-disk2)
-#gcloud compute disks list --format=json
+# Remove test and evidence disks (test-disk2 and evidence* disks)
+ZONE="us-central1-a"
+gcloud -q compute disks delete test-disk2 --zone=$ZONE
+for d in `gcloud compute disks list --uri`
+do
+  DISK=`echo $d | cut -d '/' -f 11`
+  ZONE=`echo $d | cut -d '/' -f 9`
+  if [[ $DISK == "evidence"* ]]
+  then
+    echo "Deleting $DISK in $ZONE"
+    gcloud -q compute disks delete $DISK --zone=$ZONE
+  fi
+done
 
+# Remove terraform service account
+gcloud -q iam service-accounts delete terraform@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
