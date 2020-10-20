cd10bb51b7916d584327c5781376b06da9df8b4d
Johan Berggren <jberggren@gmail.com>
2020-10-20 09:25:41 +0200

    Logfile from env

--- docker/worker/start.sh
+++ docker/worker/start.sh
@@ -6,4 +6,10 @@ then
     echo "${TURBINIA_CONF}" | base64 -d > /etc/turbinia/turbinia.conf
 fi
 
-/usr/local/bin/turbiniactl -S psqworker
+# Use log file path from environment variable is it exists, else get the path from the config.
+if [ ! -z ${TURBINIA_LOG_FILE+x} ]
+then
+    /usr/local/bin/turbiniactl -L $TURBINIA_LOG_FILE -S psqworker
+else
+    /usr/local/bin/turbiniactl -S psqworker
+fi
