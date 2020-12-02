615d909ab84d7163fa21c8ac13adaa6f250cbe3c
hacktobeer <ramsesdebeer@gmail.com>
2020-12-02 10:22:59 +0100

    Check port and address as well.

--- turbinia/client.py
+++ turbinia/client.py
@@ -1121,8 +1121,12 @@ class TurbiniaServer(object):
   def start(self):
     """Start Turbinia Server."""
     if config.PROMETHEUS_ENABLED:
-      log.info('Starting Prometheus endpoint.')
-      start_http_server(port=config.PROMETHEUS_PORT, addr=config.PROMETHEUS_ADDR)
+      if config.PROMETHEUS_PORT and config.PROMETHEUS_ADDR:
+        log.info('Starting Prometheus endpoint.')
+        start_http_server(port=config.PROMETHEUS_PORT,
+                          addr=config.PROMETHEUS_ADDR)
+      else:
+        log.info('Prometheus enabled but port or address not set!')
     log.info('Running Turbinia Server.')
     self.task_manager.run()
 
@@ -1245,7 +1249,11 @@ class TurbiniaPsqWorker(object):
   def start(self):
     """Start Turbinia PSQ Worker."""
     if config.PROMETHEUS_ENABLED:
-      log.info('Starting Prometheus endpoint.')
-      start_http_server(port=config.PROMETHEUS_PORT, addr=config.PROMETHEUS_ADDR)
+      if config.PROMETHEUS_PORT and config.PROMETHEUS_ADDR:
+        log.info('Starting Prometheus endpoint.')
+        start_http_server(port=config.PROMETHEUS_PORT,
+                          addr=config.PROMETHEUS_ADDR)
+      else:
+        log.info('Prometheus enabled but port or address not set!')
     log.info('Running Turbinia PSQ Worker.')
     self.worker.listen()
