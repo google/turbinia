10ea4eda141027abf8b5baf7196888b518160fb0
hacktobeer <ramsesdebeer@gmail.com>
2021-02-03 12:52:10 +0100

    bla

--- turbinia/__init__.py
+++ turbinia/__init__.py
@@ -21,7 +21,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
   __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-  __version__ = "unknown"
+  __version__ = "Unknown"
 
 
 def log_and_report(message, trace):
