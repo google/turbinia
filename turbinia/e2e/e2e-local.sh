06f9db0688d807b7dacc0f46673bbc0a4ffeca53
hacktobeer <ramsesdebeer@gmail.com>
2021-02-15 16:56:12 +0100

    Set evidence folder to 777

--- turbinia/e2e/e2e-local.sh
+++ turbinia/e2e/e2e-local.sh
@@ -4,13 +4,13 @@
 # The evidence processed is Chrome Browser History.
 
 echo "Create evidence folder"
-mkdir ./input
-chmod 777 ./input
+mkdir -p ./evidence
+sudo chmod 777 ./evidence
 
 echo "Download Chrome browser history artifact"
 curl https://raw.githubusercontent.com/obsidianforensics/hindsight/master/tests/fixtures/profiles/60/History > History
-tar -vzcf ./input/history.tgz History
-ls -al ./input/
+tar -vzcf ./evidence/history.tgz History
+ls -al ./evidence/
 
 echo "Startup local turbinia docker-compose stack"
 docker-compose -f ./docker/local/docker-compose.yml up -d
@@ -26,7 +26,7 @@ docker logs turbinia-server
 docker logs turbinia-worker
 
 echo "Create  Turbinia request"
-docker exec -t turbinia-server turbiniactl -r 123456789 compresseddirectory -l /input/history.tgz 
+docker exec -t turbinia-server turbiniactl -r 123456789 compresseddirectory -l /evidence/history.tgz 
 
 echo "Sleep for 60 seconds to let Turbinia process evidence"
 sleep 60s
