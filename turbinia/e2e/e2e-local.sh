39d5b80be6acf0a7ae50de74bc6d7568b30efa10
hacktobeer <ramsesdebeer@gmail.com>
2021-03-03 08:57:49 +0100

    move up.

--- turbinia/e2e/e2e-local.sh
+++ turbinia/e2e/e2e-local.sh
@@ -22,12 +22,12 @@ sleep 5s
 echo "==> Show running instances"
 docker ps -a
 
+echo "Show loop device availability in worker"
+docker exec -t turbinia-worker /sbin/losetup -a
+
 echo "==> Show container logs"
 docker logs turbinia-server
 docker logs turbinia-worker
-
-echo "Show loop device availability in worker"
-docker exec -t turbinia-worker /sbin/losetup -a
 
 echo "==> Create  Turbinia request"
 docker exec -t turbinia-server turbiniactl -r 123456789 rawdisk -l /evidence/artifact_disk.dd
