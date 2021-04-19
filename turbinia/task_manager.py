e264cfa95cdeceb16202879b9c2061ff5a47a810
Ali Memarzia <amisi@google.com>
2021-04-19 11:38:17 -0700

    Fixed job metrics collection

--- turbinia/task_manager.py
+++ turbinia/task_manager.py
@@ -318,7 +318,7 @@ class BaseTaskManager:
     final_evidence = evidence.EvidenceCollection()
     final_evidence.request_id = request_id
     self.running_jobs.append(final_job)
-
+    turbinia_jobs_total.inc()
     # Gather evidence created by every Job in the request.
     for running_job in self.running_jobs:
       if running_job.request_id == request_id:
