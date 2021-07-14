0bc1ca28ebe45fe2280f3f5b1f17c05c80147c47
hacktobeer <ramsesdebeer@gmail.com>
2021-07-14 08:11:43 +0000

    Process review comments.

--- tools/update-gcp-infra.sh
+++ tools/update-gcp-infra.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 # Turbinia GCP management script
-# This script can be used to manage a Turbinia stack deployed to GCP. The only requirement is you have 'gcloud' installed.
+# This script can be used to manage a Turbinia stack deployed to GCP. 
+# Requirements:
+# - have 'gcloud' installed.
+# - autheticate against your GCP project with "gcloud auth login"
+# - optionally have the GCP project set with "gcloud config set project [you-project-name]"
+#
 # Use --help to show you commands supported.
 
 set -o posix
-
-CMD=$1 
-INSTANCEID=$2
-ZONE=$3
-DOCKER_TAG=$4
-CONFIG_FILE=$4
+set -e
 
 SERVER_URI="us-docker.pkg.dev/osdfir-registry/turbinia/release/turbinia-server"
 WORKER_URI="us-docker.pkg.dev/osdfir-registry/turbinia/release/turbinia-worker"
 GCLOUD=`command -v gcloud`
 
+LIST_WORKERS="$GCLOUD compute instances list --filter=\"zone:$ZONE\" --filter=\"name ~ worker-$INSTANCEID\" --format=\"json(name)\" | jq -r '.[] | .name'"
+
+
 function usage { 
-  echo "Usage: $0 COMMAND INSTANCEID ZONE [DOCKER_TAG or CONFIG_FILE]" 
-  echo "COMMAND         Pick one of the commands below"
-  echo "INSTANCEID      The Turbinia deployment instance ID"
-  echo "ZONE            The GCP zone the Turbinia deployment is located"
+  echo "Usage: $0" 
+  echo "-i              The Turbinia deployment instance ID"
+  echo "-z              The GCP zone the Turbinia deployment is located"
+  echo "-c              Choose one of the commands below"
   echo
   echo "Optional arguments:"
-  echo "DOCKER_TAG      Docker image tag, eg latest or 20210606"
-  echo "CONFIG_FILE     Path to  Turbinia configuration file"
+  echo "-t              Docker image tag, eg latest or 20210606"
+  echo "-f              Path to Turbinia configuration file"
+  echo "-s              Use plain ssh instead of gcloud compute ssh"
   echo 
   echo "Commands supported:"
   echo "change-image    Change the docker image loaded by a Turbinia deployment with DOCKER_TAG"
@@ -34,8 +38,6 @@ function usage {
   echo "stop            Stop a Turbinia deployment"
   echo "update-config   Update the Turbinia configuration of a Turbinia deployment from CONFIG_FILE"
   echo
-
-  exit 1
 }
 
 function check_gcloud {
@@ -58,7 +60,7 @@ function stop {
     $GCLOUD compute instances stop turbinia-server-$INSTANCEID --zone $ZONE
 
     # Stop all workers
-    for WORKER in `$GCLOUD compute instances list --filter="zone:$ZONE" --filter="name ~ worker-$INSTANCEID" --format="json(name)" | jq -r '.[] | .name'`
+    for WORKER in `$LIST_WORKERS`
     do
         $GCLOUD compute instances stop $WORKER --zone $ZONE --async
     done
@@ -72,7 +74,7 @@ function start {
     show_infra
 
     # Start all workers
-    for WORKER in `$GCLOUD compute instances list --filter="zone:$ZONE" --filter="name ~ worker-$INSTANCEID" --format="json(name)" | jq -r '.[] | .name'`
+    for WORKER in `$LIST_WORKERS`
     do
         $GCLOUD compute instances start $WORKER --zone $ZONE --async
     done
@@ -86,9 +88,8 @@ function start {
 
 function show_config {
     # The container environment variables are *not* available in a structured format, only as a big string blob, hence the parsing...
-    show_infra
-    read -p 'Which instance? ' INSTANCE_NAME
-    $GCLOUD -q compute instances describe $INSTANCE_NAME  --format="json" --zone=$ZONE --flatten="metadata[]" | jq '.[].items[] | select(.value | contains("TURBINIA_CONF")) | .value' | sed -e 's/.*TURBINIA_CONF\\n.*value:\(.*\)\\n.*image.*/\1/' | xargs | base64 -d
+    echo "Pulling Turbinia configuration from : turbinia-server-$INSTANCEID"
+    $GCLOUD -q compute instances describe turbinia-server-$INSTANCEID  --format="json" --zone=$ZONE --flatten="metadata[]" | jq '.[].items[] | select(.value | contains("TURBINIA_CONF")) | .value' | sed -e 's/.*value\\": \\"\(.*\)\\"\\n.*image.*/\1/' | xargs | base64 -d
 }
 
 function update_config {
@@ -106,35 +107,65 @@ function show_container_logs {
     # TODO(rbdebeer) update this to use container names instead of IDs when the terraform scripts have been updated to non-random names.
     show_infra
     read -p 'Which instance? ' INSTANCE_NAME
-    $GCLOUD compute ssh $INSTANCE_NAME --zone us-central1-f --command="docker ps"
+    if [ -z $PLAIN_SSH ] 
+    then
+        $GCLOUD compute ssh -i ~/.ssh/google_compute_engine $INSTANCE_NAME --zone $ZONE --command="docker ps"
+    else
+        ssh $INSTANCE_NAME "docker ps"
+    fi
     read -p 'Which container ID? ' CONTAINER_ID
-    $GCLOUD compute ssh $INSTANCE_NAME --zone us-central1-f --command="docker logs $CONTAINER_ID"
+    if [ -z $PLAIN_SSH ] 
+    then
+        $GCLOUD compute ssh $INSTANCE_NAME --zone $ZONE --command="docker logs $CONTAINER_ID"
+    else
+        ssh $INSTANCE_NAME "docker logs $CONTAINER_ID"
+    fi
+    
 }
 
 function update_docker_image_tag {
-    for INSTANCE in `$GCLOUD compute instances list --filter="zone:$ZONE" --filter="name ~ worker-$INSTANCEID" --format="json(name)" | jq -r '.[] | .name'`
+    for INSTANCE in `$LIST_WORKERS`
     do
         $GCLOUD beta compute instances update-container $INSTANCE --zone $ZONE --container-image=$SERVER_URI:$DOCKER_TAG
     done
 
-    for INSTANCE in `$GCLOUD compute instances list --filter="zone:$ZONE" --filter="name ~ server-$INSTANCEID" --format="json(name)" | jq -r '.[] | .name'`
-    do
-        $GCLOUD beta compute instances update-container $INSTANCE --zone $ZONE --container-image=$WORKER_URI:$DOCKER_TAG
-    done
+    $GCLOUD beta compute instances update-container turbinia-server-$INSTANCEID --zone $ZONE --container-image=$WORKER_URI:$DOCKER_TAG
 }
 
-# if less than 3 arguments supplied, display usage
-if [  $# -le 2 ]
-then
-    usage
-    exit 1
-fi
+while getopts ":c:i:z:t:f:s" option; do
+   case ${option} in
+      c ) 
+         CMD=$OPTARG;;
+      i ) 
+         INSTANCEID=$OPTARG;;
+      z ) 
+         ZONE=$OPTARG;;
+      t ) 
+         DOCKER_TAG=$OPTARG;;
+      f ) 
+         CONFIG_FILE=$OPTARG;;
+      s )
+         PLAIN_SSH=1;;
+     \? ) 
+         echo "Error: Invalid usage"
+         usage
+         exit 1
+         exit;;
+   esac
+done
+shift $((OPTIND -1))
 
 # check whether user had supplied -h or --help . If yes display usage
 if [[ ( $# == "--help") ||  $# == "-h" ]]
 then
     usage
     exit 0
+fi
+
+if [ -z ${CMD} ]; then 
+    echo "Error: No command provided"
+    usage
+    exit 1
 fi
 
 # check if the gcloud binary is present
@@ -160,9 +191,19 @@ case $CMD in
         show_config
         ;;
     update-config)
+        if [ -z ${CONFIG_FILE} ]; then 
+            echo "Error: No configuration file provided"
+            usage
+            exit 1
+        fi
         update_config
         ;;
     change-image)
+        if [ -z ${DOCKER_TAG} ]; then 
+            echo "Error: No Docker image tag provided"
+            usage
+            exit 1
+        fi
         update_docker_image_tag
         ;;        
 esac
