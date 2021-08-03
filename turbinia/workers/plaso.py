be39b21dba71ecc187a6c9211847564bf2e5ace8
Aaron Peterson <aaronp@gmail.com>
2021-08-02 19:06:11 -0700

    Add logging and add --unattended flag

--- turbinia/workers/plaso.py
+++ turbinia/workers/plaso.py
@@ -17,6 +17,8 @@
 from __future__ import unicode_literals
 
 import os
+import logging
+
 from turbinia import config
 from turbinia.evidence import APFSEncryptedDisk
 from turbinia.evidence import EvidenceState as state
@@ -53,6 +55,9 @@ class PlasoTask(TurbiniaTask):
     Returns:
       String for valid Log2timeline command.
     """
+    self.result.log(
+        'Generating Plaso command line from arguments: {0!s}'.format(conf),
+        level=logging.DEBUG)
     cmd = [base_command]
     for k, v in conf.items():
       cli_args = [
@@ -127,7 +132,11 @@ class PlasoTask(TurbiniaTask):
 
     cmd.extend(['--temporary_directory', self.tmp_dir])
     cmd.extend(['--logfile', plaso_log])
+    cmd.extend(['--unattended'])
     cmd.extend([plaso_file, evidence.local_path])
+
+    self.result.log('DEBUUUUUG: command is {0!s}'.format(cmd))
+
     result.log('Running plaso as [{0:s}]'.format(' '.join(cmd)))
     self.execute(
         cmd, result, log_files=[plaso_log], new_evidence=[plaso_evidence],
