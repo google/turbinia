dd6b969beff5f759433ff49b07612a296e9c5ed0
Aaron Peterson <aaronp@gmail.com>
2021-08-03 18:41:20 -0700

    debug print __dict__

--- turbinia/workers/plaso.py
+++ turbinia/workers/plaso.py
@@ -136,7 +136,7 @@ class PlasoTask(TurbiniaTask):
     cmd.extend([plaso_file, evidence.local_path])
 
     self.result.log('DEBUUUUUG: command is {0!s}'.format(cmd))
-    self.result.log('DEBUUUUUG: evidence is {0!s}'.format(evidence))
+    self.result.log('DEBUUUUUG: evidence is {0!s}'.format(evidence.__dict__))
 
     result.log('Running plaso as [{0:s}]'.format(' '.join(cmd)))
     self.execute(
