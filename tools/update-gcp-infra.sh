80d387fb2ee59ab51f005ab5fc32774de6f8dc64
Aaron Peterson <aaronp@gmail.com>
2021-08-04 16:08:56 -0700

    Fix update-gcp-infra.sh image paths

--- tools/update-gcp-infra.sh
+++ tools/update-gcp-infra.sh
@@ -143,10 +143,10 @@ function update_docker_image_tag {
     get_workers
     for INSTANCE in $WORKERS
     do
-        $GCLOUD beta compute instances update-container $INSTANCE --zone $ZONE --container-image=$SERVER_URI:$DOCKER_TAG
+        $GCLOUD beta compute instances update-container $INSTANCE --zone $ZONE --container-image=$WORKER_URI:$DOCKER_TAG
     done
 
-    $GCLOUD beta compute instances update-container turbinia-server-$INSTANCEID --zone $ZONE --container-image=$WORKER_URI:$DOCKER_TAG
+    $GCLOUD beta compute instances update-container turbinia-server-$INSTANCEID --zone $ZONE --container-image=$SERVER_URI:$DOCKER_TAG
 }
 
 while getopts ":c:i:z:t:f:v:k:s" option; do
