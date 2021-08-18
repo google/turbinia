1c93f2fcef3e34cd098f54d91293fcd608f1b5e3
Fry <fryx0r@gmail.com>
2021-08-18 17:09:59 +1000

    Move task back to raw disks

--- turbinia/jobs/windows_acct.py
+++ turbinia/jobs/windows_acct.py
@@ -16,8 +16,10 @@
 
 from turbinia.evidence import CompressedDirectory
 from turbinia.evidence import Directory
-from turbinia.evidence import DiskPartition
 from turbinia.evidence import DockerContainer
+from turbinia.evidence import GoogleCloudDisk
+from turbinia.evidence import GoogleCloudDiskRawEmbedded
+from turbinia.evidence import RawDisk
 from turbinia.evidence import ReportText
 from turbinia.jobs import interface
 from turbinia.jobs import manager
@@ -28,7 +30,8 @@ class WindowsAccountAnalysisJob(interface.TurbiniaJob):
   """Windows Account analysis job."""
 
   evidence_input = [
-      CompressedDirectory, Directory, DiskPartition, DockerContainer
+      Directory, RawDisk, GoogleCloudDisk, GoogleCloudDiskRawEmbedded,
+      CompressedDirectory, DockerContainer
   ]
   evidence_output = [ReportText]
 
