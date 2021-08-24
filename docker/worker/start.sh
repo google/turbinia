c310f8205b8d7e5b5d06240545d9c7890bfe24e5
Aaron Peterson <aaronp@gmail.com>
2021-08-24 14:40:45 -0700

    Add TURBINIA_EXTRA_ARGS to start.sh

--- docker/worker/start.sh
+++ docker/worker/start.sh
@@ -27,7 +27,7 @@ fi
 # Use log file path from environment variable is it exists, else get the path from the config.
 if [ ! -z ${TURBINIA_LOG_FILE+x} ]
 then
-    /usr/local/bin/turbiniactl -L $TURBINIA_LOG_FILE -S $WORKER
+    /usr/local/bin/turbiniactl $TURBINIA_EXTRA_ARGS -L $TURBINIA_LOG_FILE -S $WORKER
 else
-    /usr/local/bin/turbiniactl -S $WORKER
+    /usr/local/bin/turbiniactl $TURBINIA_EXTRA_ARGS -S $WORKER
 fi
