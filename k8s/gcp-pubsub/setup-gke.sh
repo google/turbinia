5ae8a8942de06500d0eb7d9fe9c4055f0de7ac7a
hacktobeer <ramsesdebeer@gmail.com>
2021-09-23 14:17:33 +0000

    Apply k8s changes to gcp-pubsub k8s config files.

--- k8s/gcp-pubsub/setup-gke.sh
+++ k8s/gcp-pubsub/setup-gke.sh
@@ -3,6 +3,7 @@ echo "The Turbinia config file should be base64 encoded:"
 echo "$ base64 -w0 turbinia-config > turbinia-config.b64"
 echo
 
+base64 -w0 turbinia-config > turbinia-config.b64
 kubectl create configmap turbinia-config --from-file=TURBINIA_CONF=turbinia-config.b64
 kubectl create -f turbinia-server-metrics-service.yaml 
 kubectl create -f turbinia-worker-metrics-service.yaml 
