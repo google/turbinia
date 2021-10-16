ab059ba4035063b1ce8f129017f958276abc1919
Fryyyyy <Fryyyyy@users.noreply.github.com>
2021-10-17 05:04:36 +1100

    Replace MK with its text-based sibling (#927)
    * Replace MK with its text-based sibling
    Fixes #925
    
    * Fix test

--- turbinia/workers/analysis/loki_test.py
+++ turbinia/workers/analysis/loki_test.py
@@ -56,7 +56,7 @@ class LokiAnalysisTaskTest(TestTurbiniaTaskBase):
     (report, priority, summary) = self.task.runLoki(self.result, self.evidence)
     self.assertEqual(priority, 20)
     self.assertEqual(summary, self.LOKI_SUMMARY)
-    self.assertIn('mimikatz', report)
+    self.assertIn('Mimikatz', report)
 
 
 if __name__ == '__main__':
