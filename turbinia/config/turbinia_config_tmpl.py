6d21d5b2c56760a9d42f8d2db8fa80723e849faa
wajihyassine <wajihyassine@gmail.com>
2021-12-02 10:36:37 -0800

    remove parantheses

--- turbinia/config/turbinia_config_tmpl.py
+++ turbinia/config/turbinia_config_tmpl.py
@@ -49,7 +49,7 @@ TMP_DIR = '/tmp'
 # Default path to where logs will be stored. Note for a Kubernetes
 # environment, change the path to the shared path configured for Filestore
 # so that logs are can be easily retrieved from one central location.
-LOG_FILE = '/var/tmp/%s.log' % (uname().nodename)
+LOG_FILE = '/var/tmp/%s.log' % uname().nodename
 
 # Path to a lock file used for the worker tasks.
 LOCK_FILE = '%s/turbinia-worker.lock' % TMP_DIR
