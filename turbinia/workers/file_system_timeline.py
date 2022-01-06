723f8e36ab4b2dbe6ea101928c68a38ba613c52f
jleaniz <leaniz@google.com>
2022-01-06 19:33:14 +0000

    modify plaso file filters

--- turbinia/workers/file_system_timeline.py
+++ turbinia/workers/file_system_timeline.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Task to run dfimagetools FileEntryLister on disk partitions."""
+
 from __future__ import unicode_literals
 
 import os
