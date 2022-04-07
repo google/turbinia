a802c6342b4d405cc7c12459f748c7da9437ebb8
Wajih Yassine <wyassine@google.com>
2022-04-07 15:56:39 -0700

    fix lint issues

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,6 +1,6 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
-export default new Vuetify({});
+export default new Vuetify({})
