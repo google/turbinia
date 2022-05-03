b82a3f20531f2a502f1c49e5d1fee87f16b0eeaf
Said Eid <46205691+sa3eed3ed@users.noreply.github.com>
2022-05-03 19:56:10 +0200

    Change pubsub publisher to use googleapiclient.discovery API-client (#1052)
    * Change pubsub publisher to use googleapiclient.discovery API-client, update tests accordingly and fix cloud function config issue when not executing inside the gcf_init directory
    
    * formatting strings with .format to be consistent with the rest of the codebase

--- tools/gcf_init/deploy_gcf.py
+++ tools/gcf_init/deploy_gcf.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 
 import subprocess
+import os
 import sys
 
 from turbinia import config
@@ -17,18 +18,19 @@ else:
   function_names = ['gettasks', 'closetasks']
 
 config.LoadConfig()
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
 for cloud_function in function_names:
   print('Deploying function {0:s}'.format(cloud_function))
   cmd = (
       'gcloud --project {0:s} functions deploy {1:s} --stage-bucket {2:s} '
       '--region {3:s} --runtime nodejs14 --trigger-http --memory 256MB '
-      '--timeout 60s'.format(
+      '--timeout 60s --source {4:s}'.format(
           config.TURBINIA_PROJECT, cloud_function, config.BUCKET_NAME,
-          config.TURBINIA_REGION))
+          config.TURBINIA_REGION, current_dir))
   print(subprocess.check_call(cmd, shell=True))
 
 print('/nCreating Datastore index from {0:s}'.format(index_file))
 cmd = 'gcloud --quiet --project {0:s} datastore indexes create {1:s}'.format(
-    config.TURBINIA_PROJECT, index_file)
+    config.TURBINIA_PROJECT, '{0:s}/{1:s}'.format(current_dir, index_file))
 subprocess.check_call(cmd, shell=True)
