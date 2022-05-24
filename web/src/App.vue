507375920d42e5033c8041a4d160b6afd9c7d414
Wajih Yassine <wyassine@google.com>
2022-05-24 11:47:57 -0700

    add dark theme, update table styling

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,33 @@
+<!--
+Copyright 2022 Google Inc. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
 <template>
   <v-app id="app">
+    <v-app-bar app clipped-right flat>
+      <v-img src="/turbinia-logo.png" max-height="80" max-width="80" contain></v-img>
+      <v-tooltip right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-on:click="toggleTheme" v-bind="attrs" v-on="on">
+            <v-icon>mdi-brightness-6</v-icon>
+          </v-btn>
+        </template>
+      </v-tooltip>
+    </v-app-bar>
     <v-main>
-      <v-btn @click="getRequest">Refresh</v-btn>
-      <v-data-table :headers="headers" :items="requestSummary"></v-data-table>
+      <v-col class="text-right">
+        <v-btn @click="getRequest()">Refresh</v-btn>
+      </v-col>
+      <v-data-table :headers="headers" :items="outputRequest"></v-data-table>
     </v-main>
   </v-app>
 </template>
@@ -13,9 +38,10 @@ export default {
   name: 'app',
   data() {
     return {
+      search: '',
       headers: [
-        { text: 'Last Task Update Time', value: 'last_task_update_time' },
         { text: 'Request ID', value: 'request_id' },
+        { text: 'Last Task Update Time', value: 'last_task_update_time' },
         { text: 'Requester', value: 'requester' },
         { text: 'Total Tasks', value: 'total_tasks' },
         { text: 'Running Tasks', value: 'running_tasks' },
@@ -24,7 +50,6 @@ export default {
         { text: 'Status', value: 'status' },
       ],
       requestSummary: [],
-      message: 'Hello',
     }
   },
   methods: {
@@ -40,8 +65,8 @@ export default {
           var requestSummary = []
           for (const req in data) {
             requestSummary.push({
-              last_task_update_time: data[req].last_task_update_time,
               request_id: data[req].request_id,
+              last_task_update_time: data[req].last_task_update_time,
               requester: data[req].requester,
               total_tasks: data[req].task_count,
               running_tasks: data[req].running_tasks,
@@ -56,10 +81,15 @@ export default {
           console.log(error)
         })
     },
+    toggleTheme: function () {
+      console.log('Switching theme')
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.setItem('isDarkTheme', this.$vuetify.theme.dark.toString())
+    },
   },
   computed: {
-    reverseMsg() {
-      return ''
+    outputRequest() {
+      return this.requestSummary
     },
   },
   mounted() {
@@ -67,3 +97,12 @@ export default {
   },
 }
 </script>
+
+<style>
+html,
+body {
+  height: 100%;
+  overflow: auto;
+  font-family: 'Arial';
+}
+</style>
--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib/framework'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  options: {
+    customProperties: true,
+  },
+})
 
-export default new Vuetify({})
+const opts = { theme: { dark: false } }
+
+export default new Vuetify(opts)
