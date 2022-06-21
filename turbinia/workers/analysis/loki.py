8f9c4edb9d6aada39f60d7fd2714e2f9be349509
Fry <fryx0r@gmail.com>
2022-06-21 14:33:16 +1000

    Increase file size limit for Kinsing

--- turbinia/workers/analysis/loki.py
+++ turbinia/workers/analysis/loki.py
@@ -75,9 +75,9 @@ class LokiAnalysisTask(TurbiniaTask):
     stderr_file = os.path.join(self.output_dir, 'loki_stderr.log')
 
     cmd = [
-        'sudo', 'python', '/opt/loki/loki.py', '-w', '0', '--csv', '--intense',
-        '--noprocscan', '--dontwait', '--noindicator', '--nolevcheck',
-        '--nolisten', '-l', log_file, '-p', evidence.local_path
+        'sudo', 'python', '/opt/loki/loki.py', '-s', '10000', '--csv',
+        '--intense', '--noprocscan', '--dontwait', '--noindicator',
+        '--nolevcheck', '--nolisten', '-l', log_file, '-p', evidence.local_path
     ]
 
     (ret, result) = self.execute(
