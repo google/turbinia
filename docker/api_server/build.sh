ac415c815b463142c2796ed0be3c8ae3e0e00dcd
jleaniz <leaniz@google.com>
2022-07-25 22:56:23 +0000

    update docker build scripts

--- docker/api_server/build.sh
+++ docker/api_server/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t turbinia-api-server:$1 .
-docker tag turbinia-api-server:$1  gcr.io/oss-forensics-registry/turbinia/turbinia-api-server:$1
+docker tag turbinia-api-server:$1 us-docker.pkg.dev/osdfir-registry/turbinia/release/turbinia-api-server:$1
