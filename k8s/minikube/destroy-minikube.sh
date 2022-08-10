1d3944f261efa9656b5c94d3f5e59d8814aeebba
Wajih Yassine <wyassine@google.com>
2022-08-10 20:56:53 +0000

    updates to setup and destroy minikube scripts

--- k8s/minikube/destroy-minikube.sh
+++ k8s/minikube/destroy-minikube.sh
@@ -3,12 +3,12 @@
 # This script can be used to destroy the Turbinia infrastructure in minikube.
 
 kubectl delete configmap turbinia-config
-kubectl delete -f redis-server.yaml
-kubectl delete -f redis-service.yaml
-kubectl delete -f turbinia-autoscale-cpu.yaml 
-kubectl delete -f turbinia-server-metrics-service.yaml 
-kubectl delete -f turbinia-worker-metrics-service.yaml 
-kubectl delete -f turbinia-worker.yaml 
-kubectl delete -f turbinia-server.yaml
-kubectl delete -f turbinia-volume-claim-minikube.yaml
+kubectl delete -f ../celery/redis-server.yaml 
+kubectl delete -f ../celery/redis-service.yaml 
+kubectl delete -f ../celery/turbinia-server-metrics-service.yaml 
+kubectl delete -f ../celery/turbinia-worker-metrics-service.yaml 
+kubectl delete -f ../celery/turbinia-server.yaml 
+kubectl delete -f ../celery/turbinia-worker.yaml 
+kubectl delete -f ../celery/turbinia-autoscale-cpu.yaml 
 kubectl delete -f turbinia-volume-minikube.yaml
+kubectl delete -f turbinia-volume-claim-minikube.yaml
