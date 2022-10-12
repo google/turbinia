0c1f0b048bfda6ebf451d10a4479ea0458a826f3
Wajih Yassine <wyassine@google.com>
2022-10-12 19:08:23 +0000

    add frontend lb config, delete external dns, add managed ssl update oauth2 proxy, helper aliases

--- k8s/tools/helper.sh
+++ k8s/tools/helper.sh
@@ -2,25 +2,26 @@
 alias k=kubectl
 # Apply a yaml file
 alias kaf='kubectl apply -f'
-# Delete a deployment
-alias kdf='kubectl delete -f'
 # Exec into a pod
 alias ke='_ke(){ kubectl exec --stdin --tty $1 -- /bin/bash; unset -f _ke; }; _ke'
-# Get pods
-alias kgp='kubectl get pods'
-# Get deployment
-alias kgd='kubectl get deployment'
-# Describe resources
-alias kdp='kubectl describe pods'
-alias kdi='kubectl describe ingress'
-# Get nodes
-alias kgn='kubectl get nodes -o wide'
 # Get logs
 alias kl='kubectl logs'
 alias kl1h='kubectl logs --since 1h'
 alias kl1m='kubectl logs --since 1m'
 alias kl1s='kubectl logs --since 1s'
+# Get resources
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployment'
+alias kgs='kubectl get svc'
+alias kgi='kubectl get ingress'
+alias kgn='kubectl get nodes -o wide'
+# Describe resources
+alias kdp='kubectl describe pods'
+alias kdd='kubectl describe deployment'
+alias kds='kubectl describe svc'
+alias kdi='kubectl describe ingress'
 # Delete resources
+alias kdf='kubectl delete -f'
 alias kdeld='kubectl delete deployment'
 alias kdelcm='kubectl delete configmap'
 alias kdels='kubectl delete svc'
