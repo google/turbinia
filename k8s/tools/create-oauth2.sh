756e37517383797a5c5e1c770d81c2821eb2156d
Wajih Yassine <wyassine@google.com>
2022-10-14 21:37:49 +0000

    more fixes to oauth2 and lb config

--- k8s/tools/create-oauth2.sh
+++ k8s/tools/create-oauth2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 base64 -w0 oauth2_native.cfg > oauth2_native.b64
 kubectl create configmap oauth2-config --from-file=OAUTH2_CONF=oauth2_native.b64
-kubectl create -f ../celery/turbinia-healthcheck-backend.yaml
-kubectl create -f ../celery/turbinia-oauth2.yaml 
-kubectl create -f ../celery/turbinia-oauth2-svc.yaml
+kubectl create -f ../celery/turbinia-neg-healthcheck.yaml
+kubectl create -f ../celery/turbinia-oauth2-proxy.yaml 
+kubectl create -f ../celery/turbinia-oauth2-proxy-service.yaml
+kubectl create -f ../celery/turbinia-loadbalancer-managed-ssl.yaml
 kubectl create -f ../celery/turbinia-ingress.yaml
+kubectl create -f ../celery/turbinia-loadbalancer-frontend-config.yaml
--- k8s/tools/del-oauth2.sh
+++ k8s/tools/del-oauth2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 kubectl delete configmap oauth2-config
-kubectl delete svc turbinia-oauth2-svc
-kubectl delete deployment turbinia-oauth2-server
-kubectl delete -f ../celery/turbinia-healthcheck-backend.yaml
+kubectl delete svc turbinia-oauth2-service
+kubectl delete deployment turbinia-oauth2-proxy
+kubectl delete frontendconfig turbinia-loadbalancer-frontend-config
+kubectl delete backendconfig turbinia-neg-healthcheck
+kubectl delete managedcertificate turbinia-loadbalancer-managed-ssl
 kubectl delete ingress turbinia-ingress
