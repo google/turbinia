638494e4614c7b5f7d66f7454e852a2f88b8ccd6
Wajih Yassine <wyassine@google.com>
2022-11-04 22:56:50 +0000

    remove oauth create and delete script in favor of documentation

--- k8s/tools/del-oauth2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl delete configmap oauth2-config
-kubectl delete svc turbinia-oauth2-service
-kubectl delete deployment turbinia-oauth2-proxy
-kubectl delete frontendconfig turbinia-loadbalancer-frontend-config
-kubectl delete backendconfig turbinia-neg-healthcheck
-kubectl delete managedcertificate turbinia-loadbalancer-managed-ssl
-kubectl delete ingress turbinia-ingress
