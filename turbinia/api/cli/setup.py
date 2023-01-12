b28afad354aebe44ae293e0acbf9010c537ff934
Juan Leaniz <leaniz@google.com>
2023-01-12 23:28:42 +0000

    Bump versions

--- turbinia/api/cli/setup.py
+++ turbinia/api/cli/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 NAME = "turbinia-client"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 REQUIRES = [
     "click",
--- turbinia/api/client/setup.py
+++ turbinia/api/client/setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "turbinia-api-lib"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 # To install the library, run the following
 #
 # python setup.py install
