9530aa4081adead347f6baaa0eebf7c7c087eee5
Igor Rodrigues <igormr@google.com>
2023-06-18 16:13:36 +0000

    Updated the script to use jq

--- redis-query.sh
+++ redis-query.sh
@@ -19,7 +19,7 @@ for key in $(redis-cli --scan | head -10); do
             # Gets the Task value and split its key:value pairs into an array
             pair="${pair#"${pair%%[![:space:]]*}"}"
             if  [ "$pair"  == "$field: $field_value"  ]; then
-                echo -e "\033[0;31m$key\033[0m";
+                #echo -e "\033[0;31m$key\033[0m";
                 if [ "$show_value" == "y" ]; then echo -e "$value\n"; fi
             fi
         done
