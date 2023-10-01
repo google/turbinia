468af23e3e96865a355b1da89f12e021ce7d0fd7
hacktobeer <ramsesdebeer@gmail.com>
2023-10-01 14:34:46 +0000

    update cloudbuild config, build script and READMEs

--- docker/worker/build.sh
+++ docker/worker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t turbinia-worker:$1 .
+docker build -t turbinia-worker:$1 --target turbinia-worker .
 docker tag turbinia-worker:$1 us-docker.pkg.dev/osdfir-registry/turbinia/release/turbinia-worker:$1
