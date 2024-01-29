5e498438464f428144fbd921f5a09f44e1a9d466
hacktobeer <ramsesdebeer@gmail.com>
2024-01-29 14:41:20 +0000

    cleanup

--- docker/worker/build.sh
+++ docker/worker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t turbinia-worker:$1 --target turbinia-worker .
+docker build -t turbinia-worker:$1 .
 docker tag turbinia-worker:$1 us-docker.pkg.dev/osdfir-registry/turbinia/release/turbinia-worker:$1
