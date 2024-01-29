fef5d0468d7979e4a3397a468f43eaf4edd33139
hacktobeer <ramsesdebeer@gmail.com>
2024-01-29 14:33:23 +0000

    unwind changes

--- docker/server/build.sh
+++ docker/server/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t turbinia-server:$1 --target turbinia-server .
+docker build -t turbinia-server:$1 .
 docker tag turbinia-server:$1 us-docker.pkg.dev/osdfir-registry/turbinia/release/turbinia-server:$1
