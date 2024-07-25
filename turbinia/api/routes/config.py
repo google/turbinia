d5fa5a5720e7cfa17a413498966ea156cc3c74eb
hacktobeer <ramsesdebeer@gmail.com>
2024-07-25 10:01:49 +0200

    Add download configuration file functionality.

--- turbinia/api/routes/config.py
+++ turbinia/api/routes/config.py
@@ -18,11 +18,12 @@ import json
 import logging
 
 from fastapi import HTTPException, APIRouter
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 from fastapi.requests import Request
 
 from turbinia import __version__
 from turbinia import config as turbinia_config
+from turbinia.api import utils as api_utils
 from turbinia.api.schemas import request_options
 
 log = logging.getLogger(__name__)
@@ -58,3 +59,16 @@ async def get_version(request: Request):
   """Returns the Turbinia version."""
   response = {'version': __version__}
   return JSONResponse(content=response, status_code=200)
+
+
+@router.get('/download')
+async def download_config(request: Request):
+  """Downloads Turbinia configuration."""
+  try:
+    contents = api_utils.get_configuration_content()
+    return Response(content=contents, status_code=200)
+  except Exception as exception:
+    log.error(f'Error reading configuration file: {exception!s}')
+    raise HTTPException(
+        status_code=500,
+        detail='Error reading configuration file') from exception
