9e266b8c150f84436d22a65137a2ac845fd9c7b9
Wajih Yassine <54372074+wajihyassine@users.noreply.github.com>
2024-08-01 09:04:29 -0700

    Web UI: Add request report view (#1519)
    * Add request report view
    
    * Remove unneeded styles
    
    * Remove unneeded code
    
    * Update request details status for success

--- web/src/utils/RestApiClient.js
+++ web/src/utils/RestApiClient.js
@@ -42,6 +42,11 @@ export default {
     return RestApiClient.get('/api/result/request/' + request_id, { responseType: 'blob' })
   },
 
+  // Request Report
+  getRequestReport(request_id) {
+    return RestApiClient.get('/api/request/report?request_id=' + request_id)
+  },
+
   // Task List
   getTaskList(request_id) {
     return RestApiClient.get('/api/request/' + request_id)
